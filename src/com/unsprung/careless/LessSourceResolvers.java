c74f3a6b8b9629e33fbb4e8a586cc2bdf18a485c
raymanoz <rbarlow@raymanoz.com>
2014-09-21 15:19:32 +0100

    added LessSourceResolvers

---
+++ src/com/unsprung/careless/LessSourceResolvers.java
@@ -0,0 +1,41 @@
+package com.unsprung.careless;
+
+import com.googlecode.totallylazy.Function1;
+import com.googlecode.totallylazy.Sequence;
+import com.googlecode.totallylazy.regex.Regex;
+import org.lesscss.LessSource;
+
+import java.io.File;
+
+public class LessSourceResolvers {
+    public static Function1<String, LessSource> byJar(final String path) {
+        return new Function1<String, LessSource>() {
+            @Override
+            public LessSource call(String lessFile) throws Exception {
+                return new LessSource(lessFile, new JarURLResolver(path));
+            }
+        };
+    }
+
+    public static Function1<String, LessSource> byFile(final String path) {
+        return new Function1<String, LessSource>() {
+            @Override
+            public LessSource call(String lessFile) throws Exception {
+                return new LessSource(new File(path + lessFile));
+            }
+        };
+    }
+
+    public static Function1<String, LessSource> from(String property) {
+        final Regex regex = Regex.regex("(jar|file):(.*)");
+
+        final Sequence<String> extract = regex.extract(property);
+        final String scheme = extract.get(0);
+        if (scheme.equals("jar"))
+            return byJar(property);
+        else if (scheme.equals("file"))
+            return byFile(extract.get(1));
+
+        throw new RuntimeException("Illegal scheme: " + scheme);
+    }
+}
