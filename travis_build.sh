ad07353da18efefbb6cf29c37e037c72ab092b96
Raymond Barlow <raymond.barlow@barclays.com>
2014-09-22 22:47:56 +0100

    more attempting to publish to bintray

--- travis_build.sh
+++ travis_build.sh
@@ -6,10 +6,11 @@ source function_moan.bash
 ./jcompilo.sh
 
 version_url=https://api.bintray.com/content/raymanoz/careless/careless/${BUILD_NUMBER}
+artifact="careless-"${BUILD_NUMBER}
 
-curl --fail -T build/artifacts/careless* -uraymanoz:${BINTRAY_API_KEY} ${version_url}/ || moan "Failed to push distribution to ${version_url}/"
+cd build/artifacts
+curl --fail -T "{"${artifact}".jar,"${artifact}".pom,"${artifact}"-sources.jar}" -uraymanoz:${BINTRAY_API_KEY} ${version_url}/ || moan "Failed to push distribution to ${version_url}/"
 echo
 
 curl --fail -X POST -uraymanoz:${BINTRAY_API_KEY} ${version_url}/publish || moan "Failed to publish distribution with ${version_url}/publish"
 echo
-
