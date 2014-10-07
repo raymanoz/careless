ed06d78ded6b1a8b5bb820e9e90c56cfd1a7d56a
raymanoz <rbarlow@raymanoz.com>
2014-10-07 23:16:06 +0100

    some debug

--- travis_build.sh
+++ travis_build.sh
@@ -17,7 +17,9 @@ cd build/artifacts
 
 function publish_to_raymanoz() {
     echo "This is a test" > test.txt
+    echo $(s3cmd --version)
     s3cmd --access_key=${AWS_ACCESS_KEY} --secret_key=${AWS_SECRET_KEY} put --acl-public --add-header=Cache-Control:"public, max-age=3600" --guess-mime-type test.txt s3://repo.raymanoz.com/test.txt
+    echo "s3cmd done"
 }
 
 function publish_to_bintray() {
@@ -27,9 +29,9 @@ function publish_to_bintray() {
     curl --fail -X POST -uraymanoz:${BINTRAY_API_KEY} ${repo}/careless/${BUILD_NUMBER}/publish || moan "Failed to publish distribution with ${version_url}/publish"
     echo
 
-    publish_to_raymanoz
+    publish_to_raymanoz || moan 'Failed to publish to raymanoz'
 }
 
 if [[ "${TRAVIS_BRANCH}" == 'master' && "${TRAVIS_PULL_REQUEST}" == 'false' ]]; then
-    publish_to_bintray
+    publish_to_bintray || moan 'Failed to publish to bintray'
 fi
