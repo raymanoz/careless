a10e9d7b684413f9ed4b2e609e4b32d337135505
Raymond Barlow <rbarlow@raymanoz.com>
2015-04-26 21:48:43 +0100

    Upgrading jcompilo, to get the latest shavenmaven, with deps publishing--I hope :-/

--- jcompilo.sh
+++ jcompilo.sh
@@ -2,7 +2,7 @@
 
 JAVA_OPTS=-Djava.net.useSystemProxies=true ${JAVA_OPTS}
 BUILD_NUMBER=${BUILD_NUMBER-dev.build}
-version=162
+version=1.6
 artifact=jcompilo
 group=com/googlecode/${artifact}
 repo=repo.bodar.com
@@ -14,14 +14,14 @@ remote_file=${url}.pack.gz
 remote_sh=${url}.sh
 
 if [ "$1" = "update" ]; then
-	rm ${jar} ${pack}
+	rm -f ${jar} ${pack}
 fi
 
 if [ ! -f ${jar} ]; then
 	mkdir -p ${dir}
 	wget -O ${pack} ${remote_file} || curl -o ${pack} ${remote_file}
 	unpack200 ${pack} ${jar}
-	rm ${pack}
+	rm -f ${pack}
 	#wget -O $0 ${remote_sh} || curl -o $0 ${remote_sh}
 fi
 exec java -showversion -Dbuild.number=${BUILD_NUMBER} ${JAVA_OPTS} -jar ${jar} $*
